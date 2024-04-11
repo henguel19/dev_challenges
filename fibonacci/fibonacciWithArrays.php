a5db64acb8e837e5ac58f2d5ed2df2ee3c0c22eb
Henguelbert Loyo <henguel19@gmail.com>
2024-04-10 23:21:52 -0600

    Create fibonacciWithArrays.php
    create first 50 fibonacci series numbers using arrays in php

---
+++ fibonacci/fibonacciWithArrays.php
@@ -0,0 +1,17 @@
+<?php
+  $fibonacciSerie = [];
+  
+  function fillFibonacciSerie(&$seriesContainer) {
+    if (count($seriesContainer) == 0) {
+      $seriesContainer[] = 0;
+      $seriesContainer[] = 1;
+    }
+    
+    for ($index = count($seriesContainer); $index <= 50; $index++) {
+      $seriesContainer[$index] = $seriesContainer[$index-2] + $seriesContainer[$index-1];
+    }
+  }
+  
+  fillFibonacciSerie($fibonacciSerie);
+  print_r($fibonacciSerie);
+?>
