5c4a98308e8465775e3d8742ef643d883a7bf37e
Henguelbert Loyo <henguel19@gmail.com>
2024-04-10 21:44:59 -0600

    Fix anagram logic to match with requirement
    add validation to avoid equal words

--- anagram/anagram.php
+++ anagram/anagram.php
@@ -1,6 +1,7 @@
 <?php
   function isAnagram($word1, $word2) {
-    return strlen($word1) == strlen($word2) &&
+    return $word1 !== $word2 &&
+      strlen($word1) == strlen($word2) &&
       array_diff(str_split($word1), str_split($word2)) == [] &&
       array_diff(str_split($word2), str_split($word1)) == []
       ? "Yes" : "No";
@@ -9,4 +10,5 @@
   echo "Are roma & amor anagram words? " . isAnagram("roma", "amor") . "<br/>";
   echo "Are sol & sola anagram words? " . isAnagram("sol", "sola") . "<br/>";
   echo "Are paz & zap anagram words? " . isAnagram("paz", "zap") . "<br/>";
+  echo "Are bye & bye anagram words? " . isAnagram("bye", "bye") . "<br/>";
 ?>
